e248e39760683aba7d3f70cb6ae5be1ee03d06ef
peshala perera <peshala.20201256@iit.ac.lk>
2023-01-07 10:31:23 +0530

    gallery fetch data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ fetch("properties.json")
     let latest_properties_output = "";
     let gallery_output = "";
     let our_properties_output = "";
+    let our_gallery = "";
 
     for (let property of properties) {
       latest_properties_output += `<div class="carousel-item-b swiper-slide">
@@ -94,11 +95,11 @@ fetch("properties.json")
       </div>
       </div>`;
 
-      our_properties_output += ` <div class="col-md-4">
+      our_properties_output += `<div class="col-md-4">
       <div class="card-box-a card-shadow">
         <div class="img-box-a">
           <img
-            src="assets/img/properties/prop1.jpg"
+            src="assets/img/${property.picture}"
             alt=""
             class="img-a img-fluid"
           />
@@ -149,11 +150,42 @@ fetch("properties.json")
         </div>
       </div>
       </div>`;
+
+      our_gallery += `<div class="col-md-4">
+      <div class="card-box-b card-shadow news-box">
+        <div class="img-box-b">
+          <img
+            src="assets/img/${property.picture}"
+            alt=""
+            class="img-b img-fluid"
+          />
+        </div>
+        <div class="card-overlay">
+          <div class="card-header-b">
+            <div class="card-category-b">
+              <a href="blog-single.html" class="category-b">${property.type}</a>
+            </div>
+            <div class="card-title-b">
+              <h2 class="title-2">
+                <a href="blog-single.html"
+                  >Travel is comming <br />
+                  new</a
+                >
+              </h2>
+            </div>
+            <div class="card-date">
+              <span class="date-b">18 Sep. 2017</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
     }
 
     document.querySelector("#latest-properties-output").innerHTML = latest_properties_output;
     document.querySelector("#gallery-output").innerHTML = gallery_output;
     document.querySelector("#our-properties-output").innerHTML = our_properties_output;
+    document.querySelector("#our-gallery").innerHTML = our_gallery;
   })
   .catch(function (err) {
     console.log(err);
