bac8dd595473c9e0190e7a78f17e15bd631b85df
bhavan <bhavanl@techprovint.com>
2023-01-07 17:55:13 +0530

    file based rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,42 @@
-async function getProperties() {
-    const data = await fetch('properties.json');
-    const properties = await data.json();
-    return properties;
-}
-
 function getLatestPropertiesView(property) {
     return `
         <div class="carousel-item-b swiper-slide">
             <div class="card-box-a card-shadow">
                 <div class="img-box-a">
-                    <img src="assets/img/${property.picture}" alt="" class="img-a img-fluid" />
+                    <img src="assets/img/${property.picture}" alt="${property.title}" class="img-a img-fluid" />
                 </div>
                 <div class="card-overlay">
                     <div class="card-overlay-a-content">
                         <div class="card-header-a">
                             <h2 class="card-title-a">
-                                <a href="property-single.html"
-                                    >206 Mount <br />
-                                    Olive Road Two</a
-                                >
+                                <a href="property-single.html">${property.title}</a>
                             </h2>
                         </div>
                         <div class="card-body-a">
                             <div class="price-box d-flex">
-                                <span class="price-a">rent | $ 12.000</span>
+                                <span class="price-a">${property.status} | $${property.price}</span>
                             </div>
-                            <a href="#" class="link-a"
-                                >View Property
-                                <span class="bi bi-chevron-right"></span>
+                            <a href="#" class="link-a">
+                                View Property <span class="bi bi-chevron-right"></span>
                             </a>
                         </div>
                         <div class="card-footer-a">
                             <ul class="card-info d-flex justify-content-around">
                                 <li>
                                     <h4 class="card-info-title">Area</h4>
-                                    <span
-                                        >340m
-                                        <sup>2</sup>
-                                    </span>
+                                    <span>${property.area}<sup>2</sup></span>
                                 </li>
                                 <li>
                                     <h4 class="card-info-title">Beds</h4>
-                                    <span>2</span>
+                                    <span>${property.bedrooms}</span>
                                 </li>
                                 <li>
                                     <h4 class="card-info-title">Baths</h4>
-                                    <span>4</span>
+                                    <span>${property.baths}</span>
                                 </li>
                                 <li>
                                     <h4 class="card-info-title">Garages</h4>
-                                    <span>1</span>
+                                    <span>${property.garages}</span>
                                 </li>
                             </ul>
                         </div>
@@ -65,7 +52,7 @@ function getGalleryView(property) {
         <div class="carousel-item-c swiper-slide">
             <div class="card-box-b card-shadow news-box">
                 <div class="img-box-b">
-                    <img src="assets/img/${property.picture}" alt="" class="img-b img-fluid" />
+                    <img src="assets/img/${property.picture}" alt="${property.title}" class="img-b img-fluid"/>
                 </div>
                 <div class="card-overlay">
                     <div class="card-header-b">
@@ -74,14 +61,11 @@ function getGalleryView(property) {
                         </div>
                         <div class="card-title-b">
                             <h2 class="title-2">
-                                <a href="blog-single.html"
-                                    >House is comming <br />
-                                    new</a
-                                >
+                                <a href="blog-single.html">${property.title}</a>
                             </h2>
                         </div>
                         <div class="card-date">
-                            <span class="date-b">18 Sep. 2017</span>
+                            <span class="date-b">${property.added.day} ${property.added.month} ${property.added.year}</span>
                         </div>
                     </div>
                 </div>
@@ -95,52 +79,40 @@ function getOuPropertiesView(property) {
         <div class="col-md-4">
             <div class="card-box-a card-shadow">
                 <div class="img-box-a">
-                    <img src="assets/img/${property.picture}" alt="" class="img-a img-fluid" />
+                    <img src="assets/img/${property.picture}" alt="${property.title}" class="img-a img-fluid" />
                 </div>
                 <div class="card-overlay">
                     <div class="card-overlay-a-content">
                         <div class="card-header-a">
                             <h2 class="card-title-a">
-                                <a href="#"
-                                    >204 Mount <br />
-                                    Olive Road Two</a
-                                >
+                                <a href="#">${property.title}</a>
                             </h2>
                         </div>
                         <div class="card-body-a">
                             <div class="price-box d-flex">
-                                <span class="price-a">rent | $ 12.000</span>
+                                <span class="price-a">${property.status} | $${property.price}</span>
                             </div>
-                            <a href="property-single.html" class="link-a"
-                                >Click here to view
-                                <span class="bi bi-chevron-right"></span>
+                            <a href="property-single.html" class="link-a">
+                                Click here to view <span class="bi bi-chevron-right"></span>
                             </a>
                         </div>
                         <div class="card-footer-a">
                             <ul class="card-info d-flex justify-content-around">
                                 <li>
                                     <h4 class="card-info-title">Area</h4>
-                                    <span
-                                        >340m
-                                        <sup>2</sup>
-                                    </span>
+                                    <span>${property.area}<sup>2</sup></span>
                                 </li>
                                 <li>
                                     <h4 class="card-info-title">Beds</h4>
-                                    <span>2</span>
+                                    <span>${property.bedrooms}</span>
                                 </li>
                                 <li>
                                     <h4 class="card-info-title">Baths</h4>
-                                    <span>4</span>
+                                    <span>${property.baths}</span>
                                 </li>
                                 <li>
                                     <h4 class="card-info-title">Garages</h4>
-                                    <span>1</span>
-                                </li>
-                                <li>
-                                    <a href="#" class="link-one">
-                                        <i class="bi bi-heart fs-1" aria-hidden="true"></i>
-                                    </a>
+                                    <span>${property.garages}</span>
                                 </li>
                             </ul>
                         </div>
@@ -156,7 +128,7 @@ function getOurGalleyView(property) {
         <div class="col-md-4">
             <div class="card-box-b card-shadow news-box">
                 <div class="img-box-b">
-                    <img src="assets/img/${property.picture}" alt="" class="img-b img-fluid" />
+                    <img src="assets/img/${property.picture}" alt="${property.title}" class="img-b img-fluid" />
                 </div>
                 <div class="card-overlay">
                     <div class="card-header-b">
@@ -165,19 +137,15 @@ function getOurGalleyView(property) {
                         </div>
                         <div class="card-title-b">
                             <h2 class="title-2">
-                                <a href="blog-single.html"
-                                    >Travel is comming <br />
-                                    new</a
-                                >
+                                 <a href="#">${property.title}</a>
                                 <br />
-                                <a href="#" class="link-a"
-                                    >View Property
-                                    <span class="bi bi-chevron-right"></span>
+                                <a href="#" class="link-a">
+                                    View Property <span class="bi bi-chevron-right"></span>
                                 </a>
                             </h2>
                         </div>
                         <div class="card-date">
-                            <span class="date-b">18 Sep. 2017</span>
+                            <span class="date-b">${property.added.day} ${property.added.month} ${property.added.year}</span>
                         </div>
                     </div>
                 </div>
@@ -191,23 +159,18 @@ function getFavouritesView(property) {
         <div class="col-md-4">
             <div class="card-box-d">
                 <div class="card-img-d">
-                    <img src="assets/img/${property.picture}" alt="" class="img-d img-fluid" />
+                    <img src="assets/img/${property.picture}" alt="${property.title}" class="img-d img-fluid" />
                 </div>
                 <div class="card-overlay card-overlay-hover">
                     <div class="card-header-d">
                         <div class="card-title-d align-self-center">
                             <h3 class="title-d">
-                                <a href="#" class="link-two"
-                                    >Margaret Sotillo <br />
-                                    Escala</a
-                                >
+                                <a href="#"  class="link-two">${property.title}</a>
                             </h3>
                         </div>
                     </div>
                     <div class="card-body-d">
-                        <p class="content-d color-text-a">
-                            Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two.
-                        </p>
+                        <p class="content-d color-text-a">${property.description}</p>
                     </div>
                     <div class="card-footer-d">
                         <div class="socials-footer d-flex justify-content-center">
@@ -226,54 +189,90 @@ function getFavouritesView(property) {
     `;
 }
 
-async function assignProperties(propertyType) {
-    const properties = await getProperties();
+function setLatestPropertiesView(properties) {
+    const output_container = document.querySelector('#latest-properties-output');
 
+    properties.forEach(property => {
+        output_container.innerHTML += getLatestPropertiesView(property);
+    });
+}
+
+function setGalleryView(properties) {
+    const output_container = document.querySelector('#gallery-output');
+
+    properties.forEach(property => {
+        output_container.innerHTML += getGalleryView(property);
+    });
+}
+
+function setOuPropertiesView(properties) {
+    const output_container = document.querySelector('#our-properties-output');
+
+    properties.forEach(property => {
+        output_container.innerHTML += getOuPropertiesView(property);
+    });
+}
+
+function setOurGalleyView(properties) {
+    const output_container = document.querySelector('#our-gallery');
+
+    properties.forEach(property => {
+        output_container.innerHTML += getOurGalleyView(property);
+    });
+}
+
+function setFavouritesView(properties) {
+    const output_container = document.querySelector('#your-favourites');
+
+    properties.forEach(property => {
+        output_container.innerHTML += getFavouritesView(property);
+    });
+}
+
+function getCurrentFileName() {
+    var path = window.location.pathname;
+    var page = path.split('/').pop();
+    var html = page.split('.');
+    var currentFileName = html[0];
+    return currentFileName;
+}
+
+function assignProperties(propertyType, properties) {
     switch (propertyType) {
-        case 1: {
-            const output_container = document.querySelector('#latest-properties-output');
-
-            properties.forEach(property => {
-                output_container.innerHTML += getLatestPropertiesView(property);
-            });
-
+        case 'index': {
+            setLatestPropertiesView(properties);
+            setGalleryView(properties);
             break;
         }
-        case 2: {
-            const output_container = document.querySelector('#gallery-output');
-
-            properties.forEach(property => {
-                output_container.innerHTML += getGalleryView(property);
-            });
-
+        case 'find': {
+            setGalleryView(properties);
             break;
         }
-        case 3: {
-            const output_container = document.querySelector('#our-properties-output');
-
-            properties.forEach(property => {
-                output_container.innerHTML += getOuPropertiesView(property);
-            });
-
+        case 'property-grid': {
+            setOuPropertiesView(properties);
             break;
         }
-        case 4: {
-            const output_container = document.querySelector('#our-gallery');
-
-            properties.forEach(property => {
-                output_container.innerHTML += getOurGalleyView(property);
-            });
-
+        case 'gallery': {
+            setOurGalleyView(properties);
             break;
         }
-        case 5: {
-            const output_container = document.querySelector('#your-favourites');
-
-            properties.forEach(property => {
-                output_container.innerHTML += getFavouritesView(property);
-            });
-
+        case 'favourites': {
+            setFavouritesView(properties);
             break;
         }
     }
 }
+
+async function getProperties() {
+    const data = await fetch('properties.json');
+    const properties = await data.json();
+    return properties;
+}
+
+async function setPropertiesBasedOnFile() {
+    const file = getCurrentFileName();
+    const properties = await getProperties();
+    assignProperties(file, properties);
+}
+
+setPropertiesBasedOnFile();
