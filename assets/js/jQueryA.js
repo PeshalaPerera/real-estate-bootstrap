3558e16cee80115d4001a750b5a171a8b2108486
bhavan <bhavanl@techprovint.com>
2023-01-13 00:01:11 +0530

    Reformat

--- assets/js/jQueryA.js
+++ assets/js/jQueryA.js
@@ -1,92 +1,90 @@
 $(function () {
-	$("#tabs").tabs();
+    $('#tabs').tabs();
 });
 
-$(".readmore-btn").on('click', function(){
-
-    $(this).parent().toggleClass("showContent");
+$('.readmore-btn').on('click', function () {
+    $(this).parent().toggleClass('showContent');
 
     // Shorthand if-else statement
-    var replaceText = $(this).parent().hasClass("showContent") ? "Read Less" : "Read More";
+    var replaceText = $(this).parent().hasClass('showContent') ? 'Read Less' : 'Read More';
     $(this).text(replaceText);
-
 });
 
 $(function () {
-	$('#maxBedroomsSpinner').spinner({
-		min: 0,
-		max: 12,
-		spin: function (event, ui) {
-			$(this).change();
-		},
-	});
+    $('#maxBedroomsSpinner').spinner({
+        min: 0,
+        max: 12,
+        spin: function (event, ui) {
+            $(this).change();
+        },
+    });
 });
 
 $(function () {
-	$('#minBedroomsSpinner').spinner({
-		min: 0,
-		max: 12,
-		spin: function (event, ui) {
-			$(this).change();
-		},
-	});
+    $('#minBedroomsSpinner').spinner({
+        min: 0,
+        max: 12,
+        spin: function (event, ui) {
+            $(this).change();
+        },
+    });
 });
 
 $(function () {
-	$('#slider-range').slider({
-		range: true,
-		min: 0,
-		max: 1000,
-		values: [0, 300],
-		slide: function (event, ui) {
-			$('#priceRange').val('£' + ui.values[0] + ' - £' + ui.values[1]);
-		},
-	});
+    $('#slider-range').slider({
+        range: true,
+        min: 0,
+        max: 1000,
+        values: [0, 300],
+        slide: function (event, ui) {
+            $('#priceRange').val('£' + ui.values[0] + ' - £' + ui.values[1]);
+        },
+    });
 
-	$('#priceRange').val(
-		' £' + $(' #slider-range').slider('values', 0) + ' - £' + $('#slider-range').slider('values', 1)
-	);
+    $('#priceRange').val(
+        ' £' + $(' #slider-range').slider('values', 0) + ' - £' + $('#slider-range').slider('values', 1)
+    );
 });
 
 $(function () {
-	$('#search-property-btn').on('click', async function (e) {
-		e.preventDefault();
-		const properties = await getProperties();
+    $('#search-property-btn').on('click', async function (e) {
+        e.preventDefault();
+        const properties = await getProperties();
 
-		const searchType = $('#searchType').val();
-		const propType = $('#searchPropertyType').val();
-		const baths = $('#searchBathroomsInput').val();
-		const maxBed = $('#maxBedroomsSpinner').val();
-		const minBed = $('#minBedroomsSpinner').val();
-		const minPrice = $('#slider-range').slider('option', 'values')[0];
-		const maxPrice = $('#slider-range').slider('option', 'values')[1];
+        const searchType = $('#searchType').val();
+        const propType = $('#searchPropertyType').val();
+        const baths = $('#searchBathroomsInput').val();
+        const maxBed = $('#maxBedroomsSpinner').val();
+        const minBed = $('#minBedroomsSpinner').val();
+        const minPrice = $('#slider-range').slider('option', 'values')[0];
+        const maxPrice = $('#slider-range').slider('option', 'values')[1];
 
-		const propertySinglePlaceholderElement = document.getElementById('property_single_placeholder');
+        const propertySinglePlaceholderElement = document.getElementById('property_single_placeholder');
 
-		const filteredProperties = properties
-			.filter(property => property.id)
-			.filter(property => searchType === 'all' || property.type === propType)
-			.filter(property => propType === 'all' || property.city === propType)
-			.filter(property => baths === 'any' || property.baths >= baths)
-			.filter(property => property.price >= minPrice)
-			.filter(property => property.price <= maxPrice);
-		//TODO: some issue with these
-		// .filter(property => property.bedrooms >= minBed)
-		// .filter(property => property.bedrooms <= maxBed);
+        const filteredProperties = properties
+            .filter(property => property.id)
+            .filter(property => searchType === 'all' || property.type === propType)
+            .filter(property => propType === 'all' || property.city === propType)
+            .filter(property => baths === 'any' || property.baths >= baths)
+            .filter(property => property.price >= minPrice)
+            .filter(property => property.price <= maxPrice);
+        //TODO: some issue with these
+        // .filter(property => property.bedrooms >= minBed)
+        // .filter(property => property.bedrooms <= maxBed);
 
-		//TODO: Remove console logs
-		console.log({ searchType });
-		console.log({ propType });
-		console.log({ maxBed });
-		console.log({ minBed });
-		console.log({ baths });
-		console.log({ minPrice });
-		console.log({ maxPrice });
-		console.log({ properties });
-		console.log({ filteredProperties });
+        //TODO: Remove console logs
+        console.log({ searchType });
+        console.log({ propType });
+        console.log({ maxBed });
+        console.log({ minBed });
+        console.log({ baths });
+        console.log({ minPrice });
+        console.log({ maxPrice });
+        console.log({ properties });
+        console.log({ filteredProperties });
 
-		filteredProperties.forEach(prop => {
-			propertySinglePlaceholderElement.innerHTML += `
+        filteredProperties.forEach(prop => {
+            propertySinglePlaceholderElement.innerHTML += `
                 <div class="container">
                     <div class="card">
 						<div class="row">
@@ -125,257 +123,233 @@ $(function () {
                 </div>
 			<p></p>
             `;
-		});
-	});
-
-	//call the function
-	draggableProperty();
+        });
+        
+        //call the function
+        draggableProperty();
+    });
 });
 
-$(".property_location_draggerble").draggable({
+$('.property_location_draggerble').draggable({
+    // brings the item back to its place when dragging is over
+    revert: true,
 
-	// brings the item back to its place when dragging is over
-	revert: true,
+    // once the dragging starts, we decrease the opactiy of other items
+    // Appending a class as we do that with CSS
+    drag: function () {
+        $(this).addClass('active');
+    },
 
-	// once the dragging starts, we decrease the opactiy of other items
-	// Appending a class as we do that with CSS
-	drag: function () {
-		$(this).addClass("active");
-	},
-
-	// removing the CSS classes once dragging is over.
-	stop: function () {
-		$(this).removeClass("active");
-	}
+    // removing the CSS classes once dragging is over.
+    stop: function () {
+        $(this).removeClass('active');
+    },
 });
 
 // Adding to fav list by Droppable for the hard coded items
 var favListList = [];
-$("#favList").droppable({
+$('#favList').droppable({
+    // The class that will be appended to the to-be-dropped-element (favList)
+    activeClass: 'active',
 
-	// The class that will be appended to the to-be-dropped-element (favList)
-	activeClass: "active",
+    // The class that will be appended once we are hovering the to-be-dropped-element (favList)
+    hoverClass: 'hover',
 
-	// The class that will be appended once we are hovering the to-be-dropped-element (favList)
-	hoverClass: "hover",
+    // The acceptance of the item once it touches the to-be-dropped-element favList
+    tolerance: 'touch',
 
-	// The acceptance of the item once it touches the to-be-dropped-element favList
-	tolerance: "touch",
-	
-	drop: function (event, ui) {
-		console.log(event);
-		console.log(ui);
-		favListList.push(ui.draggable.find(".favIcon").attr("id"));
-		buildFavouriteList();
+    drop: function (event, ui) {
+        const text = ui.draggable.find('h3')?.context?.innerText;
+        favListList.push(text);
+        buildFavouriteList();
 
-		// var staffIdToAdd = ui.draggable.find(".favIcon").attr("id");
-		// var isAdded = false;
-		// var myfav = JSON.parse(localStorage.getItem("favProduct"));
-		// for (var i = 0; i < myfav.length; i++) {
-		// 	if (myfav[i] == staffIdToAdd) {
-		// 		isAdded = true;
-		// 	}
-		// }
-		// if (isAdded == false) {
-		// 	if (myfav == null) {
-		// 		myfav = [];
-		// 	}
-		// 	myfav.push(staffIdToAdd);
-		// 	localStorage.setItem("favProduct", JSON.stringify(myfav));
+        // var staffIdToAdd = ui.draggable.find(".favIcon").attr("id");
+        // var isAdded = false;
+        // var myfav = JSON.parse(localStorage.getItem("favProduct"));
+        // for (var i = 0; i < myfav.length; i++) {
+        // 	if (myfav[i] == staffIdToAdd) {
+        // 		isAdded = true;
+        // 	}
+        // }
+        // if (isAdded == false) {
+        // 	if (myfav == null) {
+        // 		myfav = [];
+        // 	}
+        // 	myfav.push(staffIdToAdd);
+        // 	localStorage.setItem("favProduct", JSON.stringify(myfav));
 
-		// 	location.reload();
-		// }
-	}
+        // 	location.reload();
+        // }
+    },
 });
 
-function buildFavouriteList()
- {
-	$("#favList").innerHTML = '';
-	for (const obj of favListList) {
-		$('#favList').append("<p>" + obj + "</p>");
-	}
- }
+function buildFavouriteList() {
+    $('#favList').innerHTML = '';
+    for (const obj of favListList) {
+        $('#favList').append('<p>' + obj + '</p>');
+    }
+}
 //------------------------------------------------------
 // Delete from fav list by Draggable the hard coded items
-$("#favList .property_location_draggerble").draggable({
+$('#favList .property_location_draggerble').draggable({
+    // brings the item back to its place when dragging is over
+    revert: true,
 
-	// brings the item back to its place when dragging is over
-	revert: true,
+    // Appending a class as we do that with CSS
+    drag: function () {
+        $(this).addClass('active');
+    },
 
-	// Appending a class as we do that with CSS
-	drag: function () {
-		$(this).addClass("active");
-	},
-
-	// removing the CSS classes once dragging is over.
-	stop: function () {
-		$(this).removeClass("active");
-	}
-	//localStorage.setItem("favProduct", JSON.stringify(myFavourites));
+    // removing the CSS classes once dragging is over.
+    stop: function () {
+        $(this).removeClass('active');
+    },
+    //localStorage.setItem("favProduct", JSON.stringify(myFavourites));
 });
 
 // Adding to fav list by Droppable for the hard coded items
-$(".deleteItemCard").droppable({
+$('.deleteItemCard').droppable({
+    // The class that will be appended to the to-be-dropped-element (favList)
+    //activeClass: "active",
 
-	// The class that will be appended to the to-be-dropped-element (favList)
-	//activeClass: "active",
+    // The class that will be appended once we are hovering the to-be-dropped-element (favList)
+    //hoverClass: "hover",
 
-	// The class that will be appended once we are hovering the to-be-dropped-element (favList)
-	//hoverClass: "hover",
+    // The acceptance of the item once it touches the to-be-dropped-element favList
+    tolerance: 'touch',
+    drop: function (event, ui) {
+        // var staffIdToAdd = ui.draggable.find(".favIcon").attr("id");
+        // var isAdded = false;
+        // var myfav = JSON.parse(localStorage.getItem("favProduct"));
+        // for (var i = 0; i < myfav.length; i++) {
+        //   if (myfav[i] == staffIdToAdd) {
+        //     isAdded = true;
+        //   }
+        // }
+        // if (isAdded == false) {
+        //   if (myfav == null) {
+        //     myfav = [];
+        //   }
+        //   myfav.push(staffIdToAdd);
+        //   localStorage.setItem("favProduct", JSON.stringify(myfav));
 
-	// The acceptance of the item once it touches the to-be-dropped-element favList
-	tolerance: "touch",
-	drop: function (event, ui) {
-		// var staffIdToAdd = ui.draggable.find(".favIcon").attr("id");
-		// var isAdded = false;
-		// var myfav = JSON.parse(localStorage.getItem("favProduct"));
-		// for (var i = 0; i < myfav.length; i++) {
-		//   if (myfav[i] == staffIdToAdd) {
-		//     isAdded = true;
-		//   }
-		// }
-		// if (isAdded == false) {
-		//   if (myfav == null) {
-		//     myfav = [];
-		//   }
-		//   myfav.push(staffIdToAdd);
-		//   localStorage.setItem("favProduct", JSON.stringify(myfav));
+        //   location.reload();
+        // }
+        //get the favicon's id which is equal to shoe id
+        var staffIdToDelete = ui.draggable.find('.deleteIcon').attr('id');
 
-		//   location.reload();
-		// }
-		//get the favicon's id which is equal to shoe id
-		var staffIdToDelete = ui.draggable.find(".deleteIcon").attr("id");
+        //Get the local storage current items
+        var myFavourites = JSON.parse(localStorage.getItem('favProduct'));
+        if (myFavourites != null) {
+            for (var j = 0; j < myFavourites.length; j++) {
+                if (myFavourites[j] == staffIdToDelete) {
+                    //delete item from the local storage
+                    myFavourites.splice(j, 1);
+                    localStorage.setItem('favProduct', JSON.stringify(myFavourites));
+                }
 
-		//Get the local storage current items
-		var myFavourites = JSON.parse(localStorage.getItem("favProduct"));
-		if (myFavourites != null) {
-			for (var j = 0; j < myFavourites.length; j++) {
-				if (myFavourites[j] == staffIdToDelete) {
-
-					//delete item from the local storage
-					myFavourites.splice(j, 1);
-					localStorage.setItem("favProduct", JSON.stringify(myFavourites));
-				}
-
-				//reload the page after delete the item to favourite list
-				if (!localStorage.getItem("reload")) {
-					// set reload to true and then reload the page 
-					localStorage.setItem("reload", "true");
-					location.reload();
-				}
-				// after reloading remove "reload" from localStorage 
-				else {
-					localStorage.removeItem("reload");
-				}
-			}
-		}
-	}
+                //reload the page after delete the item to favourite list
+                if (!localStorage.getItem('reload')) {
+                    // set reload to true and then reload the page
+                    localStorage.setItem('reload', 'true');
+                    location.reload();
+                }
+                // after reloading remove "reload" from localStorage
+                else {
+                    localStorage.removeItem('reload');
+                }
+            }
+        }
+    },
 });
 //-----------------------------------------------------------
 
-
-$("#droppable").droppable({
-	drop: function (event, ui) {
-		$(this)
-			.addClass("ui-state-highlight")
-			.find("p")
-			.html("Dropped!");
-	}
+$('#droppable').droppable({
+    drop: function (event, ui) {
+        $(this).addClass('ui-state-highlight').find('p').html('Dropped!');
+    },
 });
 
+function draggableProperty() {
+    console.log('blablabla');
+    $('.property_location_draggerble').draggable({
+        // brings the item back to its place when dragging is over
+        revert: true,
 
-function draggableProperty() {console.log('blablabla');
-	$(".property_location_draggerble").draggable({
+        // once the dragging starts, we decrease the opactiy of other items
+        // Appending a class as we do that with CSS
+        drag: function () {
+            $(this).addClass('active');
+        },
 
-		// brings the item back to its place when dragging is over
-		revert: true,
-	
-		// once the dragging starts, we decrease the opactiy of other items
-		// Appending a class as we do that with CSS
-		drag: function () {
-			$(this).addClass("active");
-		},
-	
-		// removing the CSS classes once dragging is over.
-		stop: function () {
-			$(this).removeClass("active");
-		}
-	});
+        // removing the CSS classes once dragging is over.
+        stop: function () {
+            $(this).removeClass('active');
+        },
+    });
 
-	console.log('lalalla');
+    console.log('lalalla');
 }
 
 $(function () {
-	$(".addFavourites").on("click", function () {
+    $('.addFavourites').on('click', function () {
+        try {
+            $(this).attr('disabled', true);
 
-		try {
-			$(this).attr('disabled', true);
+            var propIdToAdd = $(this).closest('p').attr('id');
 
-			var propIdToAdd = $(this).closest("p").attr("id");
+            var myFavouriteProp = JSON.parse(localStorage.getItem('favProp'));
 
-			var myFavouriteProp = JSON.parse(localStorage.getItem("favProp"));
+            if (myFavouriteProp == null) {
+                myFavouriteProp = [];
+            }
 
-			if (myFavouriteProp == null) {
-				myFavouriteProp = [];
-			}
+            if (myFavouriteProp != null) {
+                for (var j = 0; j < myFavouriteProp.length; j++) {
+                    if (propIdToAdd == myFavouriteProp[j]) {
+                        alert('This property is already in your favourites');
+                        myFavouriteProp = [];
+                    }
+                }
+            }
 
-			if (myFavouriteProp != null) {
-				for (var j = 0; j < myFavouriteProp.length; j++) {
+            myFavouriteProp.push(propIdToAdd);
 
-					if (propIdToAdd == myFavouriteProp[j]) {
-
-						alert("This property is already in your favourites");
-						myFavouriteProp = [];
-					}
-				}
-			}
-
-			myFavouriteProp.push(propIdToAdd);
-
-			localStorage.setItem("favProp", JSON.stringify(myFavouriteProp));
-
-		}
-
-		catch (e) {
-			if (e == QUOTA_EXCEEDED_ERR) {
-				console.log("Error: Local storage limit exceeds");
-			}
-
-			else {
-				console.log("ERROR: Saving to local storge.");
-			}
-		}
-	});
+            localStorage.setItem('favProp', JSON.stringify(myFavouriteProp));
+        } catch (e) {
+            if (e == QUOTA_EXCEEDED_ERR) {
+                console.log('Error: Local storage limit exceeds');
+            } else {
+                console.log('ERROR: Saving to local storge.');
+            }
+        }
+    });
 });
 
 $(function () {
-	$(".removeFavourites").on("click", function () {
+    $('.removeFavourites').on('click', function () {
+        $(this).attr('disabled', true);
 
-		$(this).attr('disabled', true);
+        var propIdToRemove = $(this).closest('p').attr('id');
 
-		var propIdToRemove = $(this).closest("p").attr("id");
+        myFavouriteProp = JSON.parse(localStorage.getItem('favProp'));
 
-		myFavouriteProp = JSON.parse(localStorage.getItem("favProp"));
+        if (myFavouriteProp != null) {
+            for (var j = 0; j < myFavouriteProp.length; j++) {
+                if (propIdToRemove == myFavouriteProp[j]) {
+                    alert('This Property has been removed');
 
+                    delete myFavouriteProp[j];
 
-		if (myFavouriteProp != null) {
-			for (var j = 0; j < myFavouriteProp.length; j++) {
+                    localStorage.setItem('favProp', JSON.stringify(myFavouriteProp));
 
-				if (propIdToRemove == myFavouriteProp[j]) {
+                    myFavouriteProp[j] = [];
+                }
+            }
+        }
 
-					alert("This Property has been removed");
-
-					delete myFavouriteProp[j];
-
-					localStorage.setItem("favProp", JSON.stringify(myFavouriteProp));
-
-					myFavouriteProp[j] = [];
-				}
-			}
-		}
-
-		if (myFavouriteProp == null) {
-			alert("You have no favourite items");
-		}
-	});
+        if (myFavouriteProp == null) {
+            alert('You have no favourite items');
+        }
+    });
 });
